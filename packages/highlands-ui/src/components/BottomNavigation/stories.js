b391096d5cb5761c0c3c0fa5f4ea6d6875dd81ed
Jacob Roberts <jacob@churchofthehighlands.com>
2019-03-29 10:40:07 -0500

    Adds state hook for value

--- packages/highlands-ui/src/components/BottomNavigation/stories.js
+++ packages/highlands-ui/src/components/BottomNavigation/stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { addDecorator, storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { decorator } from '../../../.storybook/config';
 import BottomNavigation from './BottomNavigation';
@@ -7,33 +7,39 @@ import Icon from '@material-ui/core/Icon';
 
 storiesOf('Bottom Navigation', module)
   .addDecorator(decorator)
-  .add('with icons', () => (
-    <div
-      style={{
-        height: '95vh',
-      }}
-    >
-      <BottomNavigation value='home'>
-        <BottomNavigation.Action
-          icon={<Icon>keyboard_arrow_up</Icon>}
-          value='menu'
-        />
-        <BottomNavigation.Action
-          icon={<Icon>home</Icon>}
-          value='home'
-        />
-        <BottomNavigation.Action
-          icon={<Icon>book-open-variant</Icon>}
-          value='bible'
-        />
-        <BottomNavigation.Action
-          icon={<Icon>people_outline</Icon>}
-          value='groups'
-        />
-        <BottomNavigation.Action
-          icon={<Icon>play_circle_outline</Icon>}
-          value='media'
-        />
-      </BottomNavigation>
-    </div>
-  ))
+  .add('with icons', () => {
+    const [value, setValue] = React.useState('home');
+    return (
+      <div
+        style={{
+          height: '95vh',
+        }}
+      >
+        <BottomNavigation
+          value={value}
+          onChange={(e, value) => setValue(value)}
+        >
+          <BottomNavigation.Action
+            icon={<Icon>keyboard_arrow_up</Icon>}
+            value='menu'
+          />
+          <BottomNavigation.Action
+            icon={<Icon>home</Icon>}
+            value='home'
+          />
+          <BottomNavigation.Action
+            icon={<Icon>book-open-variant</Icon>}
+            value='bible'
+          />
+          <BottomNavigation.Action
+            icon={<Icon>people_outline</Icon>}
+            value='groups'
+          />
+          <BottomNavigation.Action
+            icon={<Icon>play_circle_outline</Icon>}
+            value='media'
+          />
+        </BottomNavigation>
+      </div>
+    )
+  })
