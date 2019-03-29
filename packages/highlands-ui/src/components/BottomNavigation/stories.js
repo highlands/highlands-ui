e767bdf5fb8fcd98f6dec4ad5d12642883ffe273
Jacob Roberts <jacob@churchofthehighlands.com>
2019-03-29 12:52:53 -0500

    Updates story to show BottomNav with menu drawer

--- packages/highlands-ui/src/components/BottomNavigation/stories.js
+++ packages/highlands-ui/src/components/BottomNavigation/stories.js
@@ -3,11 +3,16 @@ import { addDecorator, storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { decorator } from '../../../.storybook/config';
 import BottomNavigation from './BottomNavigation';
-import Icon from '@material-ui/core/Icon';
+import Button from '../Button';
+import {
+  Collapse,
+  Icon,
+} from '@material-ui/core';
 
 storiesOf('Bottom Navigation', module)
   .addDecorator(decorator)
   .add('with icons', () => {
+    const [expanded, setExpanded] = React.useState(false);
     const [value, setValue] = React.useState('home');
     return (
       <div
@@ -17,19 +22,34 @@ storiesOf('Bottom Navigation', module)
       >
         <BottomNavigation
           value={value}
+          collapse={
+            <Collapse in={expanded}>
+              <div style={{height: '150px'}}>
+                <Button
+                  borderWidth='0px'
+                  content='Give'
+                  icon='chevron_right'
+                  iconAfter
+                  onClick={action('clicked')}
+                />
+              </div>
+            </Collapse>
+          }
           onChange={(e, value) => setValue(value)}
         >
-          <BottomNavigation.Action
-            icon={<Icon>keyboard_arrow_up</Icon>}
-            value='menu'
-          />
           <BottomNavigation.Action
             icon={<Icon>home</Icon>}
             value='home'
           />
           <BottomNavigation.Action
-            icon={<Icon>book-open-variant</Icon>}
+            icon={<Icon>book</Icon>}
             value='bible'
+          />
+          <BottomNavigation.Action
+            icon={<Icon>keyboard_arrow_up</Icon>}
+            onClick={() => setExpanded(!expanded)}
+            aria-expanded={expanded}
+            aria-label="More"
           />
           <BottomNavigation.Action
             icon={<Icon>people_outline</Icon>}
